a9561f55b97c84f1f9178352f462bed35d08b394
John Prado <jcprado20@icloud.com>
2022-08-31 21:35:51 -0400

    adds new-form

--- front-end/src/Components/NewPic.js
+++ front-end/src/Components/NewPic.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link, useParams } from "react-router-dom";
+
+const API = process.env.REACT_APP_API_URL;
 
 function NewPic() {
+  const [pic, setPic] = useState({
+    name: "",
+    url: "",
+    location: "",
+    is_favorite: true,
+  });
+  const navigate = useNavigate();
+
+  const addPic = (pic) => {
+    axios
+      .post(`${API}/pics`, pic)
+      .then(navigate("/pics"))
+      .catch((err) => console.error(err));
+  };
+
+  const handleTextChange = (e) => {
+    setPic({ ...pic, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPic(pic);
+  };
+
   return (
-    <div>NewPic</div>
-  )
+    <div className="new">
+      <form onSubmit={handleSubmit} className="new">
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          value={pic.name}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Name"
+          required
+        />
+          <br />
+        <label htmlFor="url">URL:</label>
+        <input
+          id="url"
+          type="text"
+          required
+          value={pic.url}
+          placeholder="URL"
+          onChange={handleTextChange}
+        />
+          <br />
+        <label htmlFor="location">Location:</label>
+        <input
+          id="location"
+          type="text"
+          name="locations"
+          value={pic.location}
+          placeholder="Location"
+          onChange={handleTextChange}
+        />
+          <br />
+        <label htmlFor="is_favorite">Favorite:</label>
+        <input
+          id="is_favorite"
+          type="text"
+          name="is_favorite"
+          value={pic.is_favorite}
+          placeholder="Favorite?"
+          onChange={handleTextChange}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
-export default NewPic
+export default NewPic;
