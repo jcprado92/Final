3c8b204be8249926300fff17606de1b2b695dd20
John Prado <jcprado20@icloud.com>
2022-09-08 19:52:57 -0400

    moms pasta

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -3,7 +3,7 @@
 INSERT INTO pics (name, url, location, is_favorite) VALUES
 ('Washington Square Park', 'https://nypost.com/wp-content/uploads/sites/2/2018/05/washington-square-park-drugs-nyu.jpg?quality=75&strip=all', 'Manhattan', true),
 ('Pursuit Hq', 'https://media-exp2.licdn.com/dms/image/C4D0BAQGB1__8MKYhMg/company-logo_200_200/0/1534437819257?e=2147483647&v=beta&t=J3UeYjPfYT40PwtJYL4-wX6YTs7-pnIeFrHXvk0BdbI', 'Queens', true),
-('Flushing Meadows Park','https://static01.nyt.com/images/2011/08/26/arts/26JPFLUSHING5/26JPFLUSHING5-articleLarge.jpg?quality=75&auto=webp&disable=upscale', 'Queens', true),
+('Flushing Meadows Park','https://www.nycgo.com/images/venues/1169/unisphere_flushing-meadows-corona-park-_queens_shelli_weiler-20190829_0129__large.jpg', 'Queens', true),
 ('New York Botanical Garden','https://www.newyorkbyrail.com/wp-content/uploads/2017/11/New-York-Botanical-Gardens.1-1200x638.jpg', 'Bronx', true),
 ('Jeffrey Deitch Gallery','https://www.artnews.com/wp-content/uploads/2016/03/18wooster.jpg', 'Manhattan', true),
 ('Barclays Center','https://pentagram-production.imgix.net/wp/592c9b36e64dd/mb-barclays-012.jpg?crop=edges&fit=crop&h=630&rect=0%2C28%2C1280%2C798&w=1200', 'Brooklyn', true),
